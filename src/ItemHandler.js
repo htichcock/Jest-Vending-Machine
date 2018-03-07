f35b24fa55463396e25586dc456b7b99bb3e480c
Ian <ianhitchcock@outlook.com>
2018-03-07 07:04:26 -0800

    Add ItemHandler logic

---
+++ src/ItemHandler.js
@@ -0,0 +1,43 @@
+module.exports = class ItemHandler {
+  constructor(rows, cols) {
+    if (
+      rows > 20 ||
+      cols > 20 ||
+      rows <= 0 ||
+      cols <= 0 ||
+      !Number.isInteger(rows) ||
+      !Number.isInteger(cols)
+    )
+      throw new Error(`Rows and columns must be postive integers below 21.
+      rows had value of ${rows}
+      cols had value of ${cols}`);
+    const alpha = [
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+      "G",
+      "H",
+      "I",
+      "J",
+      "K",
+      "L",
+      "M",
+      "N",
+      "O",
+      "P",
+      "Q",
+      "R",
+      "S",
+      "T"
+    ];
+    for (let i = 0; i < rows; i++) {
+      for (let j = 1; j <= cols; j++) {
+        const code = alpha[i] + j;
+        this[code] = new ItemSlot(code);
+      }
+    }
+  }
+};
